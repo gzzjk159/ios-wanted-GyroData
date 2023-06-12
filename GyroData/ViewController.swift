489d673fc51c6524bbba5b324f89c69a6347f527
vetto <gzzjk159@naver.com>
2023-06-12 17:29:03 +0900

    refactor: main storyboard 삭제

--- GyroData/ViewController.swift
+++ GyroData/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  GyroData
 //
 //  Created by kjs on 2022/09/16.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
